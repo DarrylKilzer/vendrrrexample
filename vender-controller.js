43eef01f08a071c363e397a08dd88e7cd2e3195b
Darryl Kilzer <darrylkilzer@gmail.com>
2018-03-07 13:43:28 -0700

    updated from class session

--- vender-controller.js
+++ vender-controller.js
@@ -1,20 +1,41 @@
-function VenderController(){
+function VenderController() {
   //private parts
-  var venderService = new VenderService()
-
+  var venderService = new VenderService();
+  var elemId = document.getElementById("money");
+  var itemsElem = document.getElementById("items");
   //items is an array we need to get from the service and give to drawItems
-  function drawItems(items){
+
+  function drawItems(items) {
+    var template = "";
+    for (var i = 0; i < items.length; i++) {
+      var item = items[i];
+      template += `
+       <div class="col-2 outline">
+           <h3>${item.name}</h3>
+           <p>${item.description}</p>
+           <p>$${item.price}</p>
+           <p>Qty: ${item.amount}</p>
+           <button class="btn btn-success" onclick="app.controllers.venderController.purchaseItem('${
+             item.id
+           }')">${item.id}</button>
+       </div>
+    `;
+    }
+    itemsElem.innerHTML = template;
     //we will take in a paramter(items) and iterate over it to build
     //a template to draw to the screen.
-
   }
 
-
   //public parts
+  this.addMoney = function() {
+    elemId.textContent = "money: $" + venderService.addMoney();
+  };
 
-  
+  this.purchaseItem = function(id) {
+    venderService.purchaseItem(id, drawItems);
+    elemId.textContent = "money: $" + venderService.getMoney();
+  };
   //we need a function to take money from our "view" and pass it to our service
 
-
-
+  venderService.onLoad(drawItems)
 }
--- vender-service.js
+++ vender-service.js
@@ -3,47 +3,68 @@ function VenderService(){
   var money = 0
 
   //our purchaseable items, add some of your own!
-  //we could add an id property to these items to display location for purchasing(A1, D4, etc)
+  //we could add an id property to these items to display location for purchasing(, D4, etc)
   var items = [
     {
       name: "Fruit Snack Gummies",
       description: "Fruit salad done right.",
       price: 1.00,
-      amount: 45
+      amount: 45,
+      id: "A1"
     },
     {
       name: "Plant Food",
       description: "Are you hungry? I could use a light snack.",
       price: 5.00,
-      amount: 20
+      amount: 20,
+      id: "A2"
     },
     {
       name: "Admiral-able Ack-Bar",
       description: "Snack like an Admiral.",
       price: .75,
-      amount: 3
+      amount: 3,
+      id: "A3"
     },
     {
       name: "Paranormal Snacktivity",
       description: "The perfect midnight snack.",
       price: .25,
-      amount: 7
+      amount: 7,
+      id: "A4"
     },
     {
       name: "The Void",
       description: "This snack eats you!",
       price: 3.25,
-      amount: 1
+      amount: 1,
+      id: "A5"
     }
   ]
 
   //public parts
-
+  this.getMoney = function(){
+    return money
+  }
   
   this.addMoney = function(){
     money += .25
+    return this.getMoney()
+  }
+  
+  this.purchaseItem = function(id, callBack) {
+    for (let i = 0; i < items.length; i++) {
+      const item = items[i];
+      if (id == item.id && money >= item.price && item.amount > 0) {
+        item.amount--
+        money -= item.price
+        callBack(items)
+      }
+    }
   }
 
-
-
+  this.onLoad = function(cb){
+    console.log(cb)
+    cb(items)
+  }
 }
